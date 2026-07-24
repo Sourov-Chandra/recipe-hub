2402dbcee840a06e57ef6f6cc92855659c26ee7f
sourov-chandra <sourovtpi@yahoo.com>
2026-07-24 08:42:19 +0600

    added the themeProvider

---
+++ src/context/themeContext.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+//   const [theme, setTheme] = useState("light");
+const [theme, setTheme] = useState(() => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("theme") || "light";
+  }
+  return "light";
+});
+
+ useEffect(() => {
+   document.documentElement.classList.toggle("dark", theme === "dark");
+ }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => useContext(ThemeContext);
