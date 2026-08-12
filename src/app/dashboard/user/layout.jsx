2dd71af7c200931413840ca8bbec2a1530aae537
sourov-chandra <sourovtpi@yahoo.com>
2026-08-12 11:53:25 +0600

    added layout to the user dashboard

---
+++ src/app/dashboard/user/layout.jsx
@@ -0,0 +1,9 @@
+import { requireRole } from "@/lib/core/session";
+
+const UserDashboardLayout = async ({ children }) => {
+  await requireRole("user");
+
+  return children;
+};
+
+export default UserDashboardLayout;
