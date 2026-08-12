520220529589cd15c3e80be2964bfd7f7eea19a6
sourov-chandra <sourovtpi@yahoo.com>
2026-08-12 23:47:29 +0600

    add the deleteRecipe function and modified the getRecipe function

--- src/lib/api/recipes.js
+++ src/lib/api/recipes.js
@@ -1,11 +1,16 @@
 import { serverFetch, serverMutation } from "../core/server";
 
 
-export async function getRecipes() {
-  return serverFetch("/recipes");
+export async function getRecipes(params = {}) {
+  return serverFetch("/recipes", params);
 }
 
 
 export async function addRecipe(recipeData) {
   return serverMutation("/recipes", "POST", recipeData);
 }
+
+export async function deleteRecipe(id) {
+  return serverMutation(`/recipes/${id}`, "DELETE");
+}
+
